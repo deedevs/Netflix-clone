72bb14dbb9a64da32d3398ea21045a400d7117fc
David Ladipo <64803207+deedevs@users.noreply.github.com>
2020-11-18 16:01:53 +0200

    successfully fecthed and displayed

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "../axios";
+import "./Row.css";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
@@ -15,8 +18,19 @@ function Row({ title, fetchUrl }) {
   }, [fetchUrl]);
 
   return (
-    <div>
+    <div className="row">
       <h1>{title}</h1>
+      <div className="row-posters">
+        {movies.map((movie) => {
+          return (
+            <img
+              className="row-poster"
+              src={`${base_url}${movie.poster_path}`}
+              alt={movie.name}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
