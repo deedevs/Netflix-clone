c58995797d5b13d9b0a11d21a49835b68b27a966
David Ladipo <64803207+deedevs@users.noreply.github.com>
2020-11-18 15:17:31 +0200

    created an instance of axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./App.css";
+import Row from "./components/Row";
+import requests from "./requests";
 
 function App() {
-  return <div className="App">Hey</div>;
+  return (
+    <div className="App">
+      <h1>Hey Whats up</h1>
+      <Row title="NETFLIX ORGINALS" fetchUrl={} />
+      <Row title="Trending Now" fetchUrl={} />
+    </div>
+  );
 }
 
 export default App;
