c19c07dc8bee06484cea09b996abe18d57139c6c
David Ladipo <64803207+deedevs@users.noreply.github.com>
2020-11-18 16:16:19 +0200

    added css to the rows

--- src/components/Row.js
+++ src/components/Row.js
@@ -24,6 +24,7 @@ function Row({ title, fetchUrl }) {
         {movies.map((movie) => {
           return (
             <img
+              key={movie.id}
               className="row-poster"
               src={`${base_url}${movie.poster_path}`}
               alt={movie.name}
